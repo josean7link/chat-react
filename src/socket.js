0392f14b20f90e2bb0a319c3c4428d37f8d5c21a
josean7link <josean7link@gmail.com>
2023-07-17 13:39:47 -0400

    (J) update

--- src/socket.js
+++ src/socket.js
@@ -1,6 +1,6 @@
 import SocketIOClient from 'socket.io-client';
 
-const url = 'http://127.0.0.1:3002';
+const url = 'http://127.0.0.1:3001';
 // const url = 'http://127.0.0.1:3001/api/ws';
 // const url = process.env.BACK_URL;
 // const url = 'api-dev.epxworldwide.com';
@@ -13,7 +13,7 @@ const socket = SocketIOClient(url, {
     transports: ["websocket"],
     upgrade: false,
     rejectUnauthorized: false,
-    path: '/api',
+    // path: '/api',
     query: {
         token: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoicGx1c01lbWJlciIsInN1YiI6MzQwLCJpYXQiOjE2ODc1ODIwNTUsImV4cCI6MTY4ODQ0NjA1NX0.r4K3uCex5S5Lmg1CrVLuKP7-QGPos9wI8E6m8tMk9eo`,
     },
